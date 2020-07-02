624a903226477ccbeba7fc45a1e54bb3a03e65f6
Brice Letcher <bletcher@ebi.ac.uk>
2020-07-02 10:52:04 +0100

    Add gmtools commit to tree plots

--- analysis/scripts/msps_dimorphism/plot_tree.R
+++ analysis/scripts/msps_dimorphism/plot_tree.R
@@ -3,6 +3,7 @@ library(argparser, quietly=TRUE)
 p <- arg_parser("Plot phylo tree")
 p <- add_argument(p, "phylo_tree", help="in Newick format")
 p <- add_argument(p, "output_fname", help="will save with extension of .pdf")
+p <- add_argument(p, "metadata", help="displayed in plot title")
 
 argv <- parse_args(p)
 
@@ -10,5 +11,5 @@ require(ape)
 require(ggtree)
 
 input_tree <- read.tree(argv$phylo_tree)
-display <- ggtree(input_tree) + ggtitle(paste("Tree: ", basename(argv$phylo_tree)))
+display <- ggtree(input_tree) + ggtitle(paste("Tree: ", basename(argv$phylo_tree), "\n",argv$metadata))
 ggsave(paste(argv$output_fname, ".pdf", sep=""), display)
