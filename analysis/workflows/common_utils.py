18e5fb6fb9f0e317a6aeeefff931dfb3da413d38
Brice Letcher <bletcher@ebi.ac.uk>
2020-07-02 16:24:26 +0100

    Better pacb_ilmn_validation workflow
    * re-functionalise pacb_ilmn_validation workflow
    * pacb_ilmn_validation workflow is gmtools commit specific
    * make_prgs pf gene_list better named

--- analysis/workflows/common_utils.py
+++ analysis/workflows/common_utils.py
@@ -1,4 +1,7 @@
-# Get gramtools commit
+from pathlib import Path
+from subprocess import run as sp_run, PIPE
+
+# Get gramtools commit through singularity container
 gmtools_commit_script = Path(config["scripts"]) / "gmtools_commit.py"
 GMTOOLS_COMMIT = sp_run(
     ["singularity", "exec", config["container"], "python3", gmtools_commit_script],
@@ -7,3 +10,9 @@ GMTOOLS_COMMIT = sp_run(
 )
 GMTOOLS_COMMIT.check_returncode()
 GMTOOLS_COMMIT = GMTOOLS_COMMIT.stdout.strip()
+
+
+def mk_output_dirs():
+    """For each variable starting with 'output', makes the directory name it holds"""
+    for variable in filter(lambda name: name.startswith("output"), dir()):
+        Path(eval(variable)).mkdir(exist_ok=True, parents=True)
