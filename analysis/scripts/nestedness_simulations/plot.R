a54f34a68a453277228f4d04e4d98dc81dded57d
Brice Letcher <bletcher@ebi.ac.uk>
2020-07-09 15:46:18 +0100

    Add plots of all genes in nestedness workflow

--- analysis/scripts/nestedness_simulations/plot.R
+++ analysis/scripts/nestedness_simulations/plot.R
@@ -8,6 +8,7 @@ p <- arg_parser("Plot genotyping performance")
 p <- add_argument(p, "input_tsv", help="")
 p <- add_argument(p, "output_dir", help="")
 p <- add_argument(p, "dataset_name", help="")
+p <- add_argument(p, "gramtools_commit", help="")
 
 #______Classifying calls____#
 get_classif <- function(has_call, is_correct){
@@ -38,42 +39,50 @@ get_precision <- function(classif){
 
 #_______Plots_______#
 #Compute and plot precision/recall
-plot_prec_recall <- function(data, title, argv, prg_name, gmtools_commit){
+plot_prec_recall <- function(data, title, argv, prg_name, gmtools_commit, faceted = TRUE){
   recalls <- data %>% group_by(nesting, err_rate, fcov) %>% summarise(metric = "recall", score = get_recall(classif))
   precisions <- data %>% group_by(nesting, err_rate, fcov) %>% summarise(metric = "precision", score = get_precision(classif))
   total <- rbind(recalls, precisions)
   avg_recall = get_recall(data$classif)
   avg_precision = get_precision(data$classif)
-  plot_title <- sprintf("dataset: %s \navg recall=%.2f avg precision=%.2f, gmtools_commit: %s",prg_name, avg_recall,avg_precision, gmtools_commit)
+  plot_title <- sprintf("dataset: %s, gramtools_commit: %s", prg_name, gmtools_commit)
+  plot_subtitle <-sprintf("average recall=%.3f average precision=%.3f", avg_recall,avg_precision)
   
-  prec_recall <- ggplot(total, aes(metric,score)) + geom_bar(aes(fill=nesting), stat="identity",position="dodge")
-  prec_recall <- prec_recall + facet_grid(cols=vars(err_rate), rows=vars(fcov), labeller = label_both) + labs(title = plot_title)
-  
-  title<-paste(argv$dataset_name,title,sep="_")
+  prec_recall <- ggplot(total, aes(metric,score)) + geom_bar(aes(fill=nesting), stat="identity",position="dodge") + 
+    labs(title = plot_title, subtitle=plot_subtitle)
+  if (faceted){
+    prec_recall <- prec_recall + facet_grid(cols=vars(err_rate), rows=vars(fcov), labeller = label_both)
+    title<-paste(argv$dataset_name,title,sep="_")
+  }
   ggsave(file.path(argv$output_dir,title),width = 10, height = 8, plot=prec_recall)
 }
 
 # Plot correctness vs metric distributions
-plot_GC <- function(data, title, argv, prg_name, gmtools_commit){
+plot_GC <- function(data, title, argv, prg_name, gmtools_commit, faceted = TRUE, log_scale = FALSE){
   correctness <- data %>% filter(classif == "TP" | classif == "FP")
-  correctness <- correctness %>% mutate(log_GC = log(GC))
+  if (log_scale)
+    correctness <- correctness %>% mutate(GC = log(GC))
   
-  plot_title <- sprintf("dataset: %s \ngenotype confidence x correctness, gmtools_commit: %s", prg_name, gmtools_commit)
-  GC_boxplot <- ggplot(correctness, aes(classif, log_GC)) + geom_boxplot(aes(fill=nesting)) + labs(title=plot_title)
-  GC_boxplot <- GC_boxplot + facet_grid(cols=vars(err_rate), rows=vars(fcov), labeller = label_both)
-  
-  title<-paste(argv$dataset_name,title,sep="_")
+  plot_title <- sprintf("dataset: %s, gramtools_commit: %s", prg_name, gmtools_commit)
+  GC_boxplot <- ggplot(correctness, aes(classif, GC)) + geom_boxplot(aes(fill=nesting)) + 
+    labs(title=plot_title) + xlab("Call classification") + ylab("Genotype confidence")
+  if (faceted){
+    GC_boxplot <- GC_boxplot + facet_grid(cols=vars(err_rate), rows=vars(fcov), labeller = label_both)
+    title<-paste(argv$dataset_name,title,sep="_")
+  }
+    
   ggsave(file.path(argv$output_dir,title),width = 8, height = 6, plot=GC_boxplot)
 }
 
 
-plot_GCP <- function(data, title, argv, prg_name, gmtools_commit){
+plot_GCP <- function(data, title, argv, prg_name, gmtools_commit, faceted = TRUE){
   correctness <- data %>% filter(classif == "TP" | classif == "FP")
   correctness <- correctness %>% mutate(log_GC = log(GC))
   
-  plot_title <- sprintf("dataset: %s\ngenotype confidence percentile  x correctness, gmtools_commit: %s", prg_name, gmtools_commit)
+  plot_title <- sprintf("dataset: %s\ngenotype confidence percentile  x correctness, gramtools_commit: %s", prg_name, gmtools_commit)
   GCP_boxplot <- ggplot(correctness, aes(classif, GCP)) + geom_boxplot(aes(fill=nesting)) + labs(title=plot_title)
-  GCP_boxplot <- GCP_boxplot + facet_grid(cols=vars(err_rate), rows=vars(fcov), labeller = label_both)
+  if (faceted)
+    GCP_boxplot <- GCP_boxplot + facet_grid(cols=vars(err_rate), rows=vars(fcov), labeller = label_both)
   
   title<-paste(argv$dataset_name,title,sep="_")
   ggsave(file.path(argv$output_dir,title),width = 8, height = 6, plot=GCP_boxplot)
@@ -91,23 +100,28 @@ print_prec_recall <- function(data){
 #_____Main code: data load, process, plot____#
 argv <- parse_args(p)
 data <- read_tsv(argv$input_tsv)
-gmtools_commit <- basename(argv$output_dir)
+gmtools_commit <- argv$gramtools_commit
 prg_name = argv$dataset_name
 
-data <- data %>% filter(prg == prg_name) 
 
 # Add classification
 data <- data %>% mutate(classif = pmap_chr(list(res_has_call, res_is_correct), get_classif))
 
-print(sprintf("Num filtered out ambiguous sites: %d out of %d",table(data$ambiguous)[2], dim(data)[1]))
+prg_data <- data %>% filter(prg == prg_name) 
+print(sprintf("Num filtered out ambiguous sites for prg %s: %d out of %d",
+              prg_name, table(prg_data$ambiguous)[2], dim(prg_data)[1]))
+
 data_noambi <- filter(data, ambiguous == 0)
-data_lvl1 <- filter(data_noambi, lvl_1 == 1)
+prg_data <- data_noambi %>% filter(prg == prg_name) 
+
+data_lvl1 <- filter(prg_data, lvl_1 == 1)
+all_data_fixed <- filter(data_noambi, err_rate==0)
 
 plot_prec_recall(data_noambi, "precision_recall.pdf", argv, prg_name, gmtools_commit)
 plot_prec_recall(data_lvl1, "precision_recall_lvl1.pdf", argv, prg_name, gmtools_commit)
+plot_prec_recall(all_data_fixed, "precision_recall_all.pdf", argv,  "all genes", gmtools_commit, faceted = FALSE)
 
 plot_GC(data_noambi, "GC_distrib.pdf", argv, prg_name, gmtools_commit)
-plot_GC(data_lvl1, "GC_distrib_lvl1.pdf", argv, prg_name, gmtools_commit)
+plot_GC(all_data_fixed, "GC_distrib_all.pdf", argv, "all genes", gmtools_commit, faceted=FALSE)
 
 plot_GCP(data_noambi, "GCP_distrib.pdf", argv, prg_name, gmtools_commit)
-plot_GCP(data_lvl1, "GCP_distrib_lvl1.pdf", argv, prg_name, gmtools_commit)
