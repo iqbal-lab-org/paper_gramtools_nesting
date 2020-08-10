8db9ded3c12a585dd6e5193cd3e0b2c2dd379502
Brice Letcher <bletcher@ebi.ac.uk>
2020-08-10 18:34:41 +0100

    Update tb bigdel initial bed production

---
+++ analysis/input_data/make_prgs/beds/scripts/bigdel_vcfs_to_bed.sh
@@ -0,0 +1,14 @@
+set -eu
+if [[ $# -ne 3 ]]; then echo "usage: $0 vcf_fofn min_del_size ofile"; exit 1; fi
+vcf_fofn=$1
+files=$(cat $vcf_fofn | tr '\n' ' ')
+min_del_size=$2
+ofile=$3
+
+origin_ofile=${ofile}.origin
+echo -e "contig\tstart\tstop\tfound_in_samples" > $origin_ofile
+
+
+for fname in $files; do
+	gunzip -c $fname | awk '{if (length($4) > '$min_del_size' && length($5) == 1) {print $0}}' | grep '1/1' | grep 'PASS'| awk '{print $1"\t"$2"\t"$2 + length($4)"\t'$(basename ${fname%%.*})'"}'
+	done | bedtools sort | bedtools merge -o distinct -c 4 | tee -a $origin_ofile | cut -f1-3 | awk '{l++;print $0"\tbigdel_"l}' > $ofile
